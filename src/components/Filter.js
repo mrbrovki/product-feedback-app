42689d9cec34fff32bb1be5dedbabf92fd50a355
Mahkamjon Raupov <mrbrovki@surfer-172-30-7-196-hotspot.internet-for-guests.com>
2022-01-17 21:42:52 +0100

    filter bug fixed

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,17 +2,31 @@ import React, {useState} from 'react'
 import style from '../styles/Filter.module.css'
 
 const Filter = ({filter, dispatch, deviceType}) => {
+  const anyIsTRue = obj => {
+    for(let el in obj ){
+      if(obj[el] === true){
+        return true
+      }
+    }
+    return false;
+}
   const handleChange = (e) => {
     if(e.target.name === 'All'){
-      dispatch({type: 'FILTER', value: {all: true}});
+      dispatch({type: 'FILTER', value: {all: true, ui: false, ux: false, enhancements: false, bug: false, feature: false}});
     }
     else{
-      dispatch({type: 'FILTER', value: {all: false, [e.target.name.toLowerCase()]: e.target.checked}});
+      if(!anyIsTRue){
+        dispatch({type: 'FILTER', value: {all: true, ui: false, ux: false, enhancements: false, bug: false, feature: false}});
+      }
+      else{
+        dispatch({type: 'FILTER', value: {all: false, [e.target.name.toLowerCase()]: e.target.checked}});
+      }
     }
   };
  const filterInputs = ['All','UI', 'UX', 'Enhancements', 'Bug', 'Feature'].map((el, index) => 
  <span key={index}>
-  <input type='checkbox' id={el} name={el} value={el} checked={filter[el.toLowerCase()]} onChange={(e) => handleChange(e)}/>
+  <input type='checkbox' id={el} name={el} value={el} checked={filter[el.toLowerCase()]}
+         onChange={(e) => handleChange(e)}/>
   <label htmlFor={el}>{el}</label>
  </span>);
  return (
