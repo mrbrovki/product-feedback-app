dd5e876f374f4472769696641584568b84df1e3f
Mahkamjon Raupov <mrbrovki@surfer-172-30-7-196-hotspot.internet-for-guests.com>
2022-01-22 03:53:19 +0100

    sort added

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,28 +1,15 @@
-import React, {useState} from 'react'
+import React from 'react'
 import style from '../styles/Filter.module.css'
 
 const Filter = ({filter, dispatch, deviceType}) => {
-  const anyIsTRue = obj => {
-    for(let el in obj ){
-      if(obj[el] === true){
-        return true
-      }
-    }
-    return false;
-}
+  const initObj = {all: true, ui: false, ux: false, enhancements: false, bug: false, feature: false};
   const handleChange = (e) => {
     if(e.target.name === 'All'){
-      dispatch({type: 'FILTER', value: {all: true, ui: false, ux: false, enhancements: false, bug: false, feature: false}});
+      dispatch({type: 'FILTER', value: initObj});
+    }else{
+      dispatch({type: 'FILTER', value: {all: false, [e.target.name.toLowerCase()]: e.target.checked}});
     }
-    else{
-      if(!anyIsTRue){
-        dispatch({type: 'FILTER', value: {all: true, ui: false, ux: false, enhancements: false, bug: false, feature: false}});
-      }
-      else{
-        dispatch({type: 'FILTER', value: {all: false, [e.target.name.toLowerCase()]: e.target.checked}});
-      }
-    }
-  };
+  }
  const filterInputs = ['All','UI', 'UX', 'Enhancements', 'Bug', 'Feature'].map((el, index) => 
  <span key={index}>
   <input type='checkbox' id={el} name={el} value={el} checked={filter[el.toLowerCase()]}
--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,12 +1,31 @@
-import React, {useContext} from 'react'
-import { Context } from './App'
+import React, {useContext, useEffect, useState} from 'react';
+import { Context } from './App';
+import styles from '../styles/FeedbackHeader.module.css';
 const Sort = () => {
- const {dispatch} = useContext(Context);
+ const {state: {sort}, dispatch} = useContext(Context);
+ const [visibility, setVisibility] = useState('hidden');
+
+ const changeVisibility = () =>{
+  setVisibility(prev => prev === 'hidden' ? 'shown': 'hidden');
+ }
+ const changeSort = (val) =>{
+  dispatch({type: 'SORT', value: val});
+ }
+ useEffect(() =>{
+  const sort = document.querySelector('#sort');
+  sort.addEventListener('click', changeVisibility);
+ }, [])
  return (
-  <section>
-   <label htmlFor='sort'>Sort by : </label>
-   
-  </section>
+   <div className={styles.sort} id='sort'>Sort by:
+   <span> {sort} </span>
+   <img src='./assets/shared/icon-arrow-down.svg'/>
+   <ul className={styles[visibility]}>
+    <li onClick={() => changeSort('most upvoted')}>most upvoted</li>
+    <li onClick={() => changeSort('least upvoted')}>least upvoted</li>
+    <li onClick={() => changeSort('most comments')}>most comments</li>
+    <li onClick={() => changeSort('least comments')}>least comments</li>
+   </ul>
+   </div>
  )
 }
 
